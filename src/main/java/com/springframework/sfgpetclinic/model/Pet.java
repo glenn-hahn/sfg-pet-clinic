05c60b89eb9c9619c382d25d9601b2a4667b4dba
glenn hahn <glenn.hahn@live.be>
2018-07-29 14:42:29 +0200

    adding POJOS. Closes #2.

---
+++ src/main/java/com/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package com.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType perType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getPerType() {
+        return perType;
+    }
+
+    public void setPerType(PetType perType) {
+        this.perType = perType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
